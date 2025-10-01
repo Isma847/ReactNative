e85ee5372632c310674c8ea27de03df258302fa1
Isma847 <castilloismael8471@gmail.com>
2025-10-01 17:56:23 -0300

    Add files via upload

---
+++ Nuevo tab.tsx
@@ -0,0 +1,10 @@
+import { ScrollView, Text } from 'react-native';
+// Este archivo va en app/(tabs)/
+export default function HomeScreen() {
+  return (
+    <ScrollView contentContainerStyle={{ paddingBottom: 80, flexGrow:1}}>
+        <Text style={{textAlign:"center", fontSize: 20, marginTop:200, color:"lightgreen", fontWeight:700}}>Esta es una pestaña nueva.</Text>
+        <Text style={{textAlign:"center", fontSize: 20, color:"lightgreen", fontWeight:700}}>:D</Text>
+    </ScrollView>
+  );
+}
